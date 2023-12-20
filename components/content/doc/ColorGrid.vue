806dfa800b21b1a1d4b9aad59bf0bb825caa9a53
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-20 15:11:20 +0100

    fix: refactor ColorGrid and ColorItem components for mobile

--- components/content/doc/ColorGrid.vue
+++ components/content/doc/ColorGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-6">
-    <div class="flex items-center px-1 gap-1 h-12 w-full rounded-md border border-muted-300 dark:border-muted-800">
+    <div class="flex flex-wrap items-center p-1 gap-1 md:h-12 w-full rounded-md border border-muted-300 dark:border-muted-800">
       <ContentSlot :use="$slots.default" unwrap="p"></ContentSlot>
     </div>
   </div>
--- components/content/doc/ColorItem.vue
+++ components/content/doc/ColorItem.vue
@@ -5,7 +5,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="h-10 flex-1 inline-flex items-center justify-center text-sm rounded-md" :class="classes">
+  <div class="h-10 px-3 flex-1 shrink-0 inline-flex items-center justify-center text-sm rounded-md" :class="classes">
     <ContentSlot :use="$slots.default" unwrap="p"></ContentSlot>
   </div>
 </template>
