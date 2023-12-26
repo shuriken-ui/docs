81f60db6f33e44d6252a63ae487519867edc9ad7
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-26 23:20:43 +0100

    feat: update docs

---
+++ components/content/examples/breakpoints/ExampleBreakpointsTable.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+const breakpoints = [
+  {
+    name: 'initial',
+    description: 'Phone, all devices',
+    value: 'min: 0px',
+  },
+  {
+    name: 'xs',
+    description: 'Phone only',
+    value: 'max: 639px',
+  },
+  {
+    name: 'sm',
+    description: 'Phone',
+    value: 'min: 640px',
+  },
+  {
+    name: 'md',
+    description: 'Tablet',
+    value: 'min: 768px',
+  },
+  {
+    name: 'ptablet',
+    description: 'Tablet (portrait)',
+    value: 'min: 768px / max: 1024px',
+  },
+  {
+    name: 'ltablet',
+    description: 'Tablet (landscape)',
+    value: 'min: 768px / max: 1024px',
+  },
+  {
+    name: 'lg',
+    description: 'Tablet',
+    value: 'min: 1025px',
+  },
+  {
+    name: 'xl',
+    description: 'Tablet',
+    value: 'min: 1280px',
+  },
+  {
+    name: '2xl',
+    description: 'Tablet',
+    value: 'min: 1536px',
+  },
+]
+</script>
+
+<template>
+  <div class="py-6">
+    <div class="rounded-md border border-muted-200 dark:border-muted-800 overflow-x-auto">
+      <table class="w-full">
+        <thead>
+          <tr>
+            <th class="bg-muted-50 px-4 py-3 text-start font-sans text-sm font-medium text-muted-800 dark:bg-muted-900 dark:text-muted-100">Size</th>
+            <th class="bg-muted-50 px-4 py-3 text-start font-sans text-sm font-medium text-muted-800 dark:bg-muted-900 dark:text-muted-100">Width</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!--Row-->
+          <tr v-for="breakpoint in breakpoints" :key="breakpoint.name" class="border-t border-muted-200 dark:border-muted-800 text-sm">
+            <td class="px-4 py-3">
+              <div class="">
+                <p>{{ breakpoint.name }}</p>
+                <p class="text-xs text-muted-400">{{ breakpoint.description }}</p>
+              </div>
+            </td>
+            <td class="px-4 py-3">
+              <div class="flex items-center flex-wrap gap-1">
+                <p>{{ breakpoint.value }}</p>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
