5b062d663d73245c6a7e25d58d0b8b2989ecb5e5
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-28 15:50:27 +0100

    feat: add BaseCard docs

---
+++ components/content/demo/card/DemoCardContrast.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex items-center justify-center px-4 pt-4 pb-0">
+    <div class="w-full bg-muted-50 dark:bg-muted-900 rounded-xl p-4 md:p-8">
+      <div class="grid max-w-3xl gap-4 md:grid-cols-2">
+        <BaseCard rounded="md" color="white" class="p-6">
+          <BaseHeading as="h4" size="sm" weight="semibold" lead="tight" class="text-muted-800 mb-2 dark:text-white"> Iam a card </BaseHeading>
+
+          <BaseParagraph size="sm" lead="tight" class="text-muted-400"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </BaseParagraph>
+        </BaseCard>
+
+        <BaseCard rounded="md" color="white-contrast" class="p-6">
+          <BaseHeading as="h4" size="sm" weight="semibold" lead="tight" class="text-muted-800 mb-2 dark:text-white"> Iam a card </BaseHeading>
+
+          <BaseParagraph size="sm" lead="tight" class="text-muted-400"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </BaseParagraph>
+        </BaseCard>
+
+        <BaseCard rounded="md" color="muted" class="p-6">
+          <BaseHeading as="h4" size="sm" weight="semibold" lead="tight" class="text-muted-800 mb-2 dark:text-white"> Iam a card </BaseHeading>
+
+          <BaseParagraph size="sm" lead="tight" class="text-muted-400"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </BaseParagraph>
+        </BaseCard>
+
+        <BaseCard rounded="md" color="muted-contrast" class="p-6">
+          <BaseHeading as="h4" size="sm" weight="semibold" lead="tight" class="text-muted-800 mb-2 dark:text-white"> Iam a card </BaseHeading>
+
+          <BaseParagraph size="sm" lead="tight" class="text-muted-400"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </BaseParagraph>
+        </BaseCard>
+      </div>
+    </div>
+  </div>
+</template>
