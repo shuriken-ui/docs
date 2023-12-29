6121c5c970ca52160b6d15416687cfba10fecbd6
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-29 14:22:22 +0100

    feat: add BasePlaceholderPage docs

---
+++ components/content/demo/placeholder/DemoPlaceholderBase.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center justify-center px-4 pt-4 pb-0">
+    <div class="w-full bg-muted-100 dark:bg-muted-900 rounded-xl p-4 md:p-6">
+      <BasePlaceholderPage title="Looks like you are new!" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim Polemonis. Duo Reges.">
+        <template #image>
+          <img class="dark:invert" src="/img/illustrations/placeholders/placeholder-1.svg" alt="placeholder-image" />
+        </template>
+
+        <div class="mt-2 flex justify-center gap-2">
+          <BaseButton color="primary" rounded="lg" class="w-40"> Take some action </BaseButton>
+        </div>
+      </BasePlaceholderPage>
+    </div>
+  </div>
+</template>
---
+++ components/content/demo/placeholder/DemoPlaceholderSlot.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center justify-center px-4 pt-4 pb-0">
+    <div class="w-full bg-muted-100 dark:bg-muted-900 rounded-xl p-4 md:p-6">
+      <BasePlaceholderPage title="Get Notified" subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim Polemonis. Duo Reges.">
+        <template #image>
+          <img class="dark:invert" src="/img/illustrations/placeholders/placeholder-3.svg" alt="placeholder-image" />
+        </template>
+
+        <div class="mt-2 flex justify-center gap-2">
+          <BaseInput
+            rounded="md"
+            placeholder="Enter your email"
+          />
+          <BaseButton color="primary" rounded="md"> Subscribe </BaseButton>
+        </div>
+      </BasePlaceholderPage>
+    </div>
+  </div>
+</template>
