fea7ecb079225b3d557e58bdf66ef6bbeb2fa229
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-31 19:18:16 +0100

    feat: add typography docs

---
+++ components/content/demo/heading/DemoHeadingBase.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center p-4">
+    <BaseHeading as="h1" size="xl" weight="semibold" lead="normal">
+      Iam a page heading
+    </BaseHeading>
+  </div>
+</template>
---
+++ components/content/demo/text/DemoTextBase.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center p-4">
+    <BaseText size="md" weight="normal" lead="normal">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urgent 
+      tamen et nihil remittunt. Et non ex maxima parte de tota iudicabis? 
+      Beatum, inquit. Ille incendat?
+    </BaseText>
+  </div>
+</template>
