b0f6b26b8d6e6cbe795a858bdb21b7a17bbe0970
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-31 18:54:45 +0100

    feat: add BaseTreeSelect docs

---
+++ components/content/demo/treeselect/DemoTreeSelectItemMedia.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="flex items-center p-4">
+    <div class="md:max-w-lg">
+      <BaseTreeSelect :children="tree" treeline>
+        <template #item-label="{ level, child, toggle }">
+          <BaseTreeSelectItem
+            :level="level"
+            :toggle="toggle"
+            :value="{
+              name: child.item.name,
+              text: child.item.text,
+              media: child.item.media,
+            }"
+          />
+        </template>
+      </BaseTreeSelect>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const tree = [
+  {
+    item: {
+      media: undefined,
+      name: 'Top Management',
+      text: undefined,
+    },
+    children: [
+      {
+        item: {
+          media: '/img/avatars/8.svg',
+          name: 'Harold Frazier',
+          text: 'Group Founder',
+        },
+        children: [],
+      },
+      {
+        item: {
+          media: '/img/avatars/5.svg',
+          name: 'Clarissa Miller',
+          text: 'Executive Director',
+        },
+      },
+    ],
+  },
+  {
+    item: {
+      media: undefined,
+      name: 'Teams',
+      text: undefined,
+    },
+    open: true,
+    children: [
+      {
+        item: {
+          media: undefined,
+          name: 'Marketing',
+          text: undefined,
+        },
+        open: true,
+        children: [
+          {
+            item: {
+              media: '/img/avatars/3.svg',
+              name: 'Clark Smith',
+              text: 'Marketing Associate',
+            },
+          },
+          {
+            item: {
+              media: '/img/avatars/2.svg',
+              name: 'Olga familly',
+              text: 'Marketing Associate',
+            },
+            children: [
+              {
+                item: {
+                  media: '/img/avatars/2.svg',
+                  name: 'Clark Smith',
+                  text: 'Marketing Associate',
+                },
+                children: [],
+              },
+              {
+                item: {
+                  media: '/img/avatars/2.svg',
+                  name: 'Olga familly',
+                  text: 'Marketing Associate',
+                },
+              },
+            ],
+          },
+          {
+            item: {
+              media: '/img/avatars/16.svg',
+              name: 'Hermann Mayer',
+              text: 'Marketing Officer',
+            },
+          },
+        ],
+      },
+      {
+        item: {
+          media: undefined,
+          name: 'Sales',
+          text: undefined,
+        },
+        children: [
+          {
+            item: {
+              media: '/img/avatars/9.svg',
+              name: 'Anna Lopez',
+              text: 'Sales Manager',
+            },
+            children: [],
+          },
+          {
+            item: {
+              media: '/img/avatars/25.svg',
+              name: 'Melany Lawfield',
+              text: 'Sales Manager',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    item: {
+      media: '/img/avatars/14.svg',
+      name: 'Jean LeBeauss',
+      text: 'Marketing Associate',
+    },
+  },
+]
+</script>
