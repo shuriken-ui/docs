192c6b37cd3c4ba1e36f7949f33889bfee45acb4
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-31 19:25:54 +0100

    feat: add BaseKbd docs

---
+++ components/content/demo/kbd/DemoKbdBase.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center gap-1 p-4">
+    <BaseKbd color="default" size="sm" rounded="md">Ctrl</BaseKbd>
+    <Icon name="lucide:plus" class="w-3 h-3" />
+    <BaseKbd color="default" size="sm" rounded="md">k</BaseKbd>
+  </div>
+</template>
