32c0737095188cd8f0b8b176d5590df8ee862023
Driss Chelouati <driss.chelouati@gmail.com>
2024-01-08 23:41:38 +0100

    feat: start react docs

---
+++ components/content/doc/InfoMessage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  icon?: string | boolean
+  type?: "default" | "primary" | "success" | "info" | "warning" | "danger" | "muted" | undefined
+}>()
+</script>
+
+<template>
+  <div class="py-6">
+    <BaseMessage :type="props.type" :icon="props.icon ? props.icon : false" rounded="md"> 
+      <ContentSlot :use="$slots.default" unwrap="p" />
+    </BaseMessage>
+  </div>
+</template>
