d5606bd7a2d25c16d1ce8f14abedb0a0c7712e9f
Driss Chelouati <driss.chelouati@gmail.com>
2024-01-08 20:03:34 +0100

    feat: update tailwind docs

---
+++ components/content/demo/focus/DemoFocusBase.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-4">
+    <div tabindex="0" class="nui-focus h-12 flex items-center px-6 rounded-md bg-muted-100 dark:bg-muted-800">
+      <span>Iam a focus ready block. Focus me!</span>
+    </div>
+  </div>
+</template>
