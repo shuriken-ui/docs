6782ddc7c62945a8b11be26a3d5bfa7ea70e1fc7
Driss Chelouati <86636408+driss-chelouati@users.noreply.github.com>
2024-03-31 21:36:36 +0000

    Documentation updates (#5)

--- components/content/demo/dropdown/DemoDropdownIconColor.vue
+++ components/content/demo/dropdown/DemoDropdownIconColor.vue
@@ -54,6 +54,58 @@
               </template>
             </BaseDropdownItem>
           </BaseDropdown>
+
+          <BaseDropdown
+            variant="button"
+            label="Dropdown"
+            orientation="start"
+          >
+            <BaseDropdownItem
+              to="#"
+              title="Profile"
+              text="View your profile"
+              color="primary"
+              rounded="sm"
+            >
+              <template #start>
+                <Icon name="ph:user-duotone" class="me-2 block h-5 w-5" />
+              </template>
+            </BaseDropdownItem>
+            <BaseDropdownItem
+              to="#"
+              title="Projects"
+              text="View your projects"
+              color="primary"
+              rounded="sm"
+            >
+              <template #start>
+                <Icon name="ph:briefcase-duotone" class="me-2 block h-5 w-5" />
+              </template>
+            </BaseDropdownItem>
+            <BaseDropdownItem
+              to="#"
+              title="Team"
+              text="Manage your team"
+              color="primary"
+              rounded="sm"
+            >
+              <template #start>
+                <Icon name="ph:users-four-duotone" class="me-2 block h-5 w-5" />
+              </template>
+            </BaseDropdownItem>
+            <BaseDropdownDivider />
+            <BaseDropdownItem
+              to="#"
+              title="Settings"
+              text="Set your preferences"
+              color="primary"
+              rounded="sm"
+            >
+              <template #start>
+                <Icon name="ph:gear-six-duotone" class="me-2 block h-5 w-5" />
+              </template>
+            </BaseDropdownItem>
+          </BaseDropdown>
         </div>
       </div>
     </div>
